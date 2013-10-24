71f20e507b7c5117775c8c3c88a6656c569832e1
trogdoro <craig.muth@gmail.com>
2013-10-23 22:50:29 -0700

    Hello World: added string.

--- src/hello_pebble.c
+++ src/hello_pebble.c
@@ -7,12 +7,13 @@
 
 #define MY_UUID { 0xFE, 0x52, 0x9C, 0xCE, 0x92, 0xF8, 0x4D, 0x26, 0xB7, 0xB6, 0xBD, 0xCE, 0x37, 0x7B, 0xA8, 0x9C }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "Timez", "The Lord",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
 Window window;
+TextLayer hello_layer;
 
 
 // When app loads...
@@ -21,6 +22,17 @@ void handle_init(AppContextRef ctx) {
 
   window_init(&window, "Window Name");
   window_stack_push(&window, true /* Animated */);
+
+  text_layer_init(&hello_layer, GRect(0, 65, 144, 30));
+  text_layer_set_text_alignment(&hello_layer, GTextAlignmentCenter);
+  text_layer_set_text(&hello_layer, "Hello Again!");
+  text_layer_set_font(&hello_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  layer_add_child(&window.layer, &hello_layer.layer);
+
+
+
+
+
 }
 
 
